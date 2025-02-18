d2b3758734506cefd2b95bee254cbaee4424db87
Qays Dwekat <qays.dwekat@gmail.com>
2025-02-18 22:54:44 +0100

    Update camera Screen

--- lib/src/screens/camera_screen.dart
+++ lib/src/screens/camera_screen.dart
@@ -1,270 +1,252 @@
-part of '../jsonschema_form_builder.dart';
+import 'dart:math';
 
-//TODO add possibility to change to front camera
-//TODO add possibility to change to use flash light
-//TODO fix issue some time buildPreview() is showing actual live camera
-class _CameraScreen extends StatefulWidget {
-  const _CameraScreen({
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:jsonschema_form/src/screens/camera_resolution.dart';
+import 'package:jsonschema_form/src/screens/camera_service.dart';
+import 'package:jsonschema_form/src/utils/xfile_extension.dart';
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({
     required this.isPhotoAllowed,
     required this.isVideoAllowed,
+    super.key,
+    this.resolution = CameraResolution.max,
   });
 
   final bool isPhotoAllowed;
   final bool isVideoAllowed;
+  final CameraResolution resolution;
 
   @override
-  State<_CameraScreen> createState() => _CameraScreenState();
+  _CameraScreenState createState() => _CameraScreenState();
 }
 
-class _CameraScreenState extends State<_CameraScreen>
+class _CameraScreenState extends State<CameraScreen>
     with WidgetsBindingObserver {
-  bool isRecording = false;
-  Timer? _timer;
-
-  CameraController? _controller;
-
-  XFile? file;
-
-  bool _isLoading = true;
+  late List<CameraDescription> cameras;
+  CameraController? _cameraController;
+  Future<void>? cameraValue;
+  bool isRecoring = false;
+  bool flash = false;
+  bool iscamerafront = true;
+  double transform = 0;
 
   @override
   void initState() {
     super.initState();
-
-    _initializeCamera();
-
+    cameras = CameraService().cameras;
+    if (cameras.isNotEmpty) {
+      _cameraController = CameraController(
+        cameras[0],
+        widget.resolution.resolutionPreset,
+      );
+      cameraValue = _cameraController?.initialize();
+    }
     WidgetsBinding.instance.addObserver(this);
-  }
-
-  void _initializeCamera() {
-    if (!mounted) return;
-
-    if (!_isLoading) {
-      setState(() {
-        _isLoading = true;
-      });
-    }
-    _controller = null;
-    availableCameras().then((cameras) {
-      _controller = CameraController(cameras[0], ResolutionPreset.max);
-      _controller?.initialize().then((_) {
-        if (!mounted) {
-          return;
-        }
-        setState(() {
-          _isLoading = false;
-        });
-        setState(() {});
-      }).catchError((Object e) {
-        if (e is CameraException) {
-          switch (e.code) {
-            case 'CameraAccessDenied':
-              break;
-            default:
-              break;
-          }
-        }
-      });
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: _controller == null
-          ? const Center(child: CircularProgressIndicator())
-          : Stack(
-              fit: StackFit.expand,
-              alignment: Alignment.center,
-              children: [
-                CameraPreview(_controller!),
-                if (isRecording)
-                  Positioned(
-                    top: 20,
-                    child: Container(
-                      padding: const EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Theme.of(context).primaryColor,
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Text(
-                        _formatSecondsToHHMMSS(_timer!.tick),
-                        style: Theme.of(context).textTheme.bodyMedium,
-                      ),
-                    ),
-                  ),
-                Positioned(
-                  left: 20,
-                  top: 20,
-                  child: IconButton(
-                    onPressed: () => Navigator.of(context).pop(),
-                    icon: const Icon(Icons.arrow_back, size: 50),
-                  ),
-                ),
-                Positioned(
-                  bottom: 50,
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      if (widget.isPhotoAllowed && widget.isVideoAllowed) ...[
-                        _buildPhotoButton(),
-                        const SizedBox(width: 10),
-                        _buildVideoButton(),
-                      ] else if (widget.isVideoAllowed)
-                        _buildVideoButton()
-                      else
-                        _buildPhotoButton(),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-    );
-  }
-
-  Widget _buildPhotoButton() => SizedBox(
-        width: 120,
-        height: 120,
-        child: ElevatedButton(
-          style:
-              const ButtonStyle(shape: WidgetStatePropertyAll(CircleBorder())),
-          onPressed: () async {
-            setState(() {
-              _isLoading = true;
-            });
-            file = await _controller!.takePicture();
-
-            if (!kIsWeb) {
-              file = await file?.rename(DateTime.now().toIso8601String());
-            }
-
-            await _buildPreviewAndAskIfDone();
-          },
-          child: const Text(
-            'Take Picture',
-            textAlign: TextAlign.center,
-          ),
-        ),
-      );
-
-  Widget _buildVideoButton() => SizedBox(
-        width: 85,
-        height: 85,
-        child: ElevatedButton(
-          style: ButtonStyle(
-            padding: const WidgetStatePropertyAll(EdgeInsets.zero),
-            backgroundColor: const WidgetStatePropertyAll(Colors.red),
-            shape: WidgetStatePropertyAll(
-              isRecording
-                  ? RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    )
-                  : const CircleBorder(),
-            ),
-          ),
-          onPressed: () async {
-            if (isRecording) {
-              _timer?.cancel();
-
-              setState(() {
-                _isLoading = true;
-              });
-
-              file = await _controller!.stopVideoRecording();
-
-              if (!kIsWeb) {
-                file = await file?.rename(DateTime.now().toIso8601String());
-              }
-
-              await _buildPreviewAndAskIfDone();
-            } else {
-              await _controller!.startVideoRecording();
-              _timer?.cancel();
-              _timer = Timer.periodic(const Duration(seconds: 1), (_) {
-                setState(() {});
-              });
-            }
-            isRecording = !isRecording;
-            setState(() {});
-          },
-          child: Text(
-            isRecording ? 'Stop' : 'Record',
-            textAlign: TextAlign.center,
-          ),
-        ),
-      );
-
-  Future<void> _buildPreviewAndAskIfDone() async {
-    final navigator = Navigator.of(context);
-
-    final widgetPreview = _controller!.buildPreview();
-
-    final isConfirmed = await showDialog<bool?>(
-      context: context,
-      builder: (context) {
-        final size = MediaQuery.sizeOf(context);
-
-        return AlertDialog(
-          title: const Text('Confirm Photo/Video'),
-          content: SizedBox(
-            width: size.width / 2,
-            height: size.height / 2,
-            child: widgetPreview,
-          ),
-          actions: [
-            TextButton(
-              onPressed: () async {
-                Navigator.of(context).pop();
-
-                await _controller!.pausePreview();
-                await _controller!.dispose();
-
-                _initializeCamera();
-              },
-              child: const Text('Retake'),
-            ),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop(true);
-              },
-              child: const Text('Confirm'),
-            ),
-          ],
-        );
-      },
-    );
-
-    if (isConfirmed != null && isConfirmed) {
-      navigator.pop(file);
-    }
-  }
-
-  String _formatSecondsToHHMMSS(int seconds) {
-    final hours = (seconds ~/ 3600).toString().padLeft(2, '0');
-    final minutes = ((seconds % 3600) ~/ 60).toString().padLeft(2, '0');
-    final secs = (seconds % 60).toString().padLeft(2, '0');
-
-    return '$hours:$minutes:$secs';
   }
 
   @override
   void didChangeAppLifecycleState(AppLifecycleState state) {
     // App state changed before we got the chance to initialize.
-    if (_controller == null || !_controller!.value.isInitialized) {
+    if (_cameraController == null || !_cameraController!.value.isInitialized) {
       return;
     }
 
     if (state == AppLifecycleState.inactive) {
-      _controller!.dispose();
-    } else if (state == AppLifecycleState.resumed) {
-      _initializeCamera();
+      _cameraController?.dispose();
     }
+    //  else if (state == AppLifecycleState.resumed) {
+    //    _initializeCamera();
+    // }
   }
 
   @override
   void dispose() {
-    _controller?.dispose();
-    _timer?.cancel();
     super.dispose();
+    _cameraController?.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          FutureBuilder(
+            future: cameraValue,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                return SizedBox(
+                  width: MediaQuery.of(context).size.width,
+                  height: MediaQuery.of(context).size.height,
+                  child: CameraPreview(_cameraController!),
+                );
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+          Positioned(
+            bottom: 0,
+            child: Container(
+              color: Colors.black,
+              padding: const EdgeInsets.only(top: 5, bottom: 5),
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          flash ? Icons.flash_on : Icons.flash_off,
+                          color: Colors.white,
+                          size: 28,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            flash = !flash;
+                          });
+                          flash
+                              ? _cameraController?.setFlashMode(FlashMode.torch)
+                              : _cameraController?.setFlashMode(FlashMode.off);
+                        },
+                      ),
+                      GestureDetector(
+                        onLongPress: () async {
+                          await _cameraController?.startVideoRecording();
+                          setState(() {
+                            isRecoring = true;
+                          });
+                        },
+                        onLongPressUp: () async {
+                          final navigator = Navigator.of(context);
+                          var videopath =
+                              await _cameraController?.stopVideoRecording();
+                          setState(() {
+                            isRecoring = false;
+                          });
+
+                          if (!kIsWeb) {
+                            videopath = await videopath
+                                ?.rename(DateTime.now().toIso8601String());
+                          }
+
+                          navigator.pop(videopath);
+                        },
+                        onTap: () {
+                          if (!isRecoring) takePhoto(context);
+                        },
+                        child: isRecoring
+                            ? const Icon(
+                                Icons.radio_button_on,
+                                color: Colors.red,
+                                size: 80,
+                              )
+                            : const Icon(
+                                Icons.panorama_fish_eye,
+                                color: Colors.white,
+                                size: 70,
+                              ),
+                      ),
+                      if ((cameras.length ?? 0) > 1)
+                        IconButton(
+                          icon: Transform.rotate(
+                            angle: transform,
+                            child: const Icon(
+                              Icons.flip_camera_ios,
+                              color: Colors.white,
+                              size: 28,
+                            ),
+                          ),
+                          onPressed: () async {
+                            setState(() {
+                              iscamerafront = !iscamerafront;
+                              transform = transform + pi;
+                            });
+                            final cameraPos = iscamerafront ? 0 : 1;
+                            _cameraController = CameraController(
+                              cameras[cameraPos],
+                              ResolutionPreset.high,
+                            );
+                            cameraValue = _cameraController?.initialize();
+                          },
+                        )
+                      else
+                        const SizedBox(
+                          width: 50,
+                        ),
+                    ],
+                  ),
+                  const SizedBox(
+                    height: 4,
+                  ),
+                  const Text(
+                    'Hold for Video, tap for photo',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> takePhoto(BuildContext context) async {
+    final navigator = Navigator.of(context);
+    var file = await _cameraController?.takePicture();
+
+    if (!kIsWeb) {
+      file = await file?.rename(DateTime.now().toIso8601String());
+    }
+
+    navigator.pop(file);
+  }
+}
+
+/// Extension for converting [CameraResolution] to [ResolutionPreset].
+///
+/// This extension provides a way to convert a [CameraResolution] value into
+/// its corresponding [ResolutionPreset] value. It is useful when you need to
+/// map different camera resolutions to preset values that
+/// the camera API can use.
+///
+/// Example usage:
+/// ```dart
+/// CameraResolution resolution = CameraResolution.high;
+/// ResolutionPreset preset = resolution.resolutionPreset;
+/// ```
+extension CameraResolutionExt on CameraResolution {
+  /// Converts the [CameraResolution] to the corresponding [ResolutionPreset].
+  ///
+  /// This method maps the different camera resolution levels
+  /// (low, medium, high, etc.) to the corresponding preset values available
+  /// in the [ResolutionPreset] enum.
+  ///
+  /// Returns the appropriate [ResolutionPreset]
+  /// based on the current [CameraResolution].
+  ResolutionPreset get resolutionPreset {
+    switch (this) {
+      case CameraResolution.low:
+        return ResolutionPreset.low;
+      case CameraResolution.medium:
+        return ResolutionPreset.medium;
+      case CameraResolution.high:
+        return ResolutionPreset.high;
+      case CameraResolution.veryHigh:
+        return ResolutionPreset.veryHigh;
+      case CameraResolution.ultraHigh:
+        return ResolutionPreset.ultraHigh;
+      case CameraResolution.max:
+        return ResolutionPreset.max;
+    }
   }
 }
